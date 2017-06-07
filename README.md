2a7cd06e8bef961e183b4495710a37777c90d58a
Spencer Tranter <spencertranter95@gmail.com>
2017-06-07 11:13:39 -0700

    A search function to use with Semios databases. Instructions in the README.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # searchRedis
+
+---
+
+The commmand line prompt is as follows:
+
+npm run start --database='trap_photos_by_mac' --key='001C2C1B26097F0B' --redisbase='latest'
+
+
+database: Which redis collection you are looking to search through.
+
+key: What the collection is keyed on. Usually a mac address.
+
+redisbase: Either 'latest' or 'general'.
+
+---
+
+This requires a config file that @stranter can give.
---
+++ index.js
@@ -0,0 +1,77 @@
+const waterfall = require('async.waterfall');
+const Redis     = require('redis');
+const config    = require('./config.json');
+
+var env         = 'prod';
+var db          = process.env.npm_config_database;
+var key         = process.env.npm_config_key
+var redisBase   = process.env.npm_config_redisbase;
+
+
+var redis;
+
+waterfall([
+
+  function (next) {
+
+    redis = Redis.createClient(config[env].redis[redisBase]);
+
+    redis.on('error', (err) => {
+
+      console.log('Unable to connect to Redis.', err);
+
+      next(err);
+
+    });
+
+    redis.on('connect', () => {
+
+      console.log('Connected to Redis.');
+
+      next(null);
+
+    });
+
+  },
+  function (next) {
+
+    console.log('Setting Redis Client Name (searchRedis).');
+    // easy to debug identity when executing CLIENT LIST.
+    redis.send_command('CLIENT', ['SETNAME', 'searchRedis'], (err) => {
+
+      if (err) next(err);
+      else     next(null);
+
+    });
+
+  },
+  function (next) {
+
+    console.log(`searching for ${key}`);
+    // gather all records
+
+    redis.hmget(db, key,  (error, reply) => {
+
+      if (error) next(error);
+      else if (reply[0] === null) next('NO DATA');
+      else {
+
+        console.log(reply);
+
+        next(null);
+
+      }
+
+    });
+
+  }
+], (err) => {
+
+
+  console.log('Disconnecting from Redis..');
+  redis.end(true);
+
+  if (err) console.log(err);
+  else console.log('done');
+
+});
